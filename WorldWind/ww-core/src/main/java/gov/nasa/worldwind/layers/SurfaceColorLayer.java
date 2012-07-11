b8c8d9ac524920ae6c6e9ecaacdb713ce9c1ef3f
Wiehann Matthysen <wcmatthysen@gmail.com>
2012-07-11 09:16:10 +0200

    Added a SurfaceColorLayer.
    - Added a dedicated layer class to simulate what was done in the
      DimGlobeSurface example. This class is constructed with a Color value
      (defaults to black if none is specified) and is used to create a
      surface-image that is stretched across the entire globe. This is
      particularly useful if you want to create a Dimming effect by setting
      this color to black, and playing around with the layer's opacity
      value (or the image's opacity for that matter).
    - Will see if I can change the DimGlobeSurface example to make use of
      this class instead of manually setting up the components.

---
+++ WorldWind/ww-core/src/main/java/gov/nasa/worldwind/layers/SurfaceColorLayer.java
@@ -0,0 +1,42 @@
+package gov.nasa.worldwind.layers;
+
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.render.SurfaceImage;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Wiehann Matthysen
+ */
+public final class SurfaceColorLayer extends SurfaceImageLayer
+{
+    private SurfaceImage image;
+    
+    public SurfaceColorLayer(Color color)
+    {
+        image = new SurfaceImage(createColorImage(color), Sector.FULL_SPHERE);
+        addRenderable(image);
+    }
+    
+    public SurfaceColorLayer()
+    {
+        this(Color.BLACK);
+    }
+    
+    public void setColor(Color color)
+    {
+        image.setImageSource(createColorImage(color), Sector.FULL_SPHERE);
+    }
+    
+    private static BufferedImage createColorImage(Color color)
+    {
+        BufferedImage image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB); 
+        Graphics2D graphics = (Graphics2D) image.getGraphics(); 
+        graphics.setColor(color);
+        graphics.fillRect(0, 0, image.getWidth(), image.getHeight()); 
+        graphics.dispose();
+        return image;
+    }
+}

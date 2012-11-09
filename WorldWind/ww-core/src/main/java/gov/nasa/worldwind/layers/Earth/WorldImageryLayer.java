ad0acbebf9db2661ecc0e13849defadbce15b5b6
Wiehann Matthysen <wcmatthysen@gmail.com>
2012-11-09 12:17:32 +0200

    Added missing license.

--- WorldWind/ww-core/src/main/java/gov/nasa/worldwind/layers/Earth/WorldImageryLayer.java
+++ WorldWind/ww-core/src/main/java/gov/nasa/worldwind/layers/Earth/WorldImageryLayer.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2011 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
 package gov.nasa.worldwind.layers.Earth;
 
 import gov.nasa.worldwind.avlist.AVKey;

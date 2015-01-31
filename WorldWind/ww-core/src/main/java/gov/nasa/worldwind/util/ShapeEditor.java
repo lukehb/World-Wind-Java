b2f20de87ed5e39ebe4eb08799b4501bd30813af
Wiehann Matthysen <wcmatthysen@gmail.com>
2015-01-31 19:14:24 +0200

    Updated license-header in sources.

--- WorldWind/ww-core/src/main/java/gov/nasa/worldwind/util/ShapeEditor.java
+++ WorldWind/ww-core/src/main/java/gov/nasa/worldwind/util/ShapeEditor.java
@@ -1,9 +1,8 @@
-/*
- * Copyright (C) 2014 United States Government as represented by the Administrator of the
- * National Aeronautics and Space Administration.
+/**
+ * Copyright (C) 2014, United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration,
  * All Rights Reserved.
  */
-
 package gov.nasa.worldwind.util;
 
 import gov.nasa.worldwind.*;

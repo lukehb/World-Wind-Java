fdcd2e89e07eb5ffc92551115a768394fdb42c4a
Wiehann Matthysen <wcmatthysen@gmail.com>
2015-05-12 13:38:07 +0200

    Screen-selector deep-picking bug.
    Fixed a bug in the ScreenSelector class where only the top-level objects
    were selected after a group-selection was made. The fix for this was to
    change the selected() method's body so that it manually constructed the
    list of all objects from the SelectEvent instead of just using the
    top-objects (via getAllTopObjects()). The ScreenSelector was also
    enhanced in that it now stores the deepPickEnabled state of the
    scene-controller and enables deep-picking when enabled, and restores it
    once disabled (see the enable() and disable() methods).

--- WorldWind/ww-extensions/src/main/java/gov/nasa/worldwindx/examples/util/ScreenSelector.java
+++ WorldWind/ww-extensions/src/main/java/gov/nasa/worldwindx/examples/util/ScreenSelector.java
@@ -309,6 +309,8 @@ public class ScreenSelector extends WWObjectImpl implements MouseListener, Mouse
     protected boolean autoEnable;
     protected boolean autoDisable;
     protected boolean armed;
+    
+    protected boolean deepPickEnabled;
 
     public ScreenSelector(WorldWindow worldWindow)
     {
@@ -334,6 +336,8 @@ public class ScreenSelector extends WWObjectImpl implements MouseListener, Mouse
         this.autoEnable = false;
         this.autoDisable = false;
         this.armed = false;
+        
+        this.deepPickEnabled = this.getWwd().getSceneController().isDeepPickEnabled();
         
         // Listen for mouse input on the World Window.
         this.getWwd().getInputHandler().addMouseListener(this);
@@ -406,6 +410,10 @@ public class ScreenSelector extends WWObjectImpl implements MouseListener, Mouse
         // ScreenSelector starts with the correct state when enabled.
         this.selectionRect.clearSelection();
         this.getWwd().getSceneController().setPickRectangle(null);
+        
+        // Save and set deep-picking to true (allows overlapping objects to be selected).
+        this.deepPickEnabled = this.getWwd().getSceneController().isDeepPickEnabled();
+        this.getWwd().getSceneController().setDeepPickEnabled(true);
 
         // Add and enable the layer that displays this ScreenSelector's selection rectangle.
         LayerList layers = this.getWwd().getModel().getLayers();
@@ -427,6 +435,9 @@ public class ScreenSelector extends WWObjectImpl implements MouseListener, Mouse
         this.selectionRect.clearSelection();
         this.getWwd().getSceneController().setPickRectangle(null);
         this.getWwd().removeSelectListener(this);
+        
+        // Restores deep-picking to previously saved-state.
+        this.wwd.getSceneController().setDeepPickEnabled(this.deepPickEnabled);
 
         // Remove the layer that displays this ScreenSelector's selection rectangle.
         this.getWwd().getModel().getLayers().remove(this.getLayer());
@@ -699,7 +710,20 @@ public class ScreenSelector extends WWObjectImpl implements MouseListener, Mouse
             {
                 // Respond to box rollover select events when armed.
                 if (event.getEventAction().equals(SelectEvent.BOX_ROLLOVER))
-                    this.selectObjects(event.getAllTopObjects());
+                {
+                    List<Object> selected = null;
+                    if (event.hasObjects())
+                    {
+                        PickedObjectList pickedObjects = event.getObjects();
+                        selected = new ArrayList<Object>(pickedObjects.size());
+                        for (PickedObject pickedObject : pickedObjects)
+                        {
+                            Object object = pickedObject.getObject();
+                            selected.add(object);
+                        }
+                    }
+                    this.selectObjects(selected);
+                }
             }
             catch (Exception e)
             {
